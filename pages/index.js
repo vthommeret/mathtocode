92a183f53dc5e73745d226a915f955eb57c4ad64
Vernon Thommeret <vernon@practice.community>
2020-05-27 12:41:41 -0400

    basic styles for javascript interpreter

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container min-h-screen m-auto flex flex-col justify-center items-center bg-gray-300 font-sans">
+    <>
       <Head>
         <title>Math to Code</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="font-semibold text-center">Math to Code</h1>
-    </div>
+      <div className="container min-h-screen m-auto flex flex-col justify-center items-center bg-black text-white font-sans">
+        <div class="font-mono m-16">
+          <p className="text-center text-lg outline-none" contentEditable>Math to Code</p>
+        </div>
+        <button class="px-2 py-1 bg-green-300 text-black font-medium rounded">Submit answer</button>
+      </div>
+    </>
   )
 }
