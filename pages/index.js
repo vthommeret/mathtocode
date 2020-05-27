2655eac25afcd7c05c68a3fc06654544eb4bf337
Vernon Thommeret <vernon@practice.community>
2020-05-27 12:21:21 -0400

    fix default fonts/spacing removed with purgecss

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container min-h-screen m-auto flex flex-col justify-center items-center bg-gray-300">
+    <div className="container min-h-screen m-auto flex flex-col justify-center items-center bg-gray-300 font-sans">
       <Head>
         <title>Math to Code</title>
         <link rel="icon" href="/favicon.ico" />
