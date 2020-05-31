dedc18db631ca28654bd19030557ea7e00906375
Vernon Thommeret <vernon@practice.community>
2020-05-31 12:00:58 -0400

    fix favicon definition

--- pages/index.js
+++ pages/index.js
@@ -111,7 +111,7 @@ const Home = ({ questions }) => {
         <title>Math to Code</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       </Head>
 
       <div className="container-shadow md:max-w-screen-md md:mt-10 md:flex md:mx-auto">
